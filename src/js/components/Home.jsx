570df1d08c7a3359a56a41e2e6afc8a5c8581da7
JuanFelipeR <juanfeliperiost@gmail.com>
2025-04-30 16:30:21 -0500

    Tasklist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,62 @@
+import { useState } from "react";
 import React from "react";
+import ReactDOM from "react-dom/client";
+import "../../styles/index.css";
+//Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export default function Home() {
+  const [inputTask, setInpuntTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && inputTask.trim() !== "") {
+      // Agrega la tarea al presionar Enter si el input no está vacío
+      setTasks([...tasks, inputTask]);
+      setInpuntTask(""); // Limpia el campo de entrada
+    }
+  };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+  const handleDelete = (index) => {
+    // Elimina la tarea al hacer clic en el botón de eliminar
+    if (index > -1) {
+      const newTasks = [...tasks];
+      newTasks.splice(index, 1);
+      setTasks(newTasks);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="container mt-4">
+		<h1 className="text-center">Task List</h1>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Add a new task"
+          value={inputTask}
+          onChange={(e) => setInpuntTask(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <ul className="list-group mt-3">
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className="sticky-note d-flex justify-content-between align-items-center"
+            >
+              <span>
+                {index + 1}.{task}
+              </span>{" "}
+              {/* Sin espacio después del punto */}
+              <div
+                onClick={() => handleDelete(index)}
+                style={{ cursor: "pointer", color: "red" }}
+              >
+                x
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
